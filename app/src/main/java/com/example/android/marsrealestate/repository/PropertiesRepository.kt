17e781f5e7ccbb4958af4a54f08e69edc8753948
Pouya Mazloumpour <mazloumpour.pouya@gmail.com>
2020-06-09 23:51:57 +0430

    refactor: fix not showing properties issue

--- app/src/main/java/com/example/android/marsrealestate/repository/PropertiesRepository.kt
+++ app/src/main/java/com/example/android/marsrealestate/repository/PropertiesRepository.kt
@@ -1,11 +1,13 @@
 package com.example.android.marsrealestate.repository
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Transformations
 import com.example.android.marsrealestate.database.PropertiesDatabase
 import com.example.android.marsrealestate.database.asDomainModel
 import com.example.android.marsrealestate.domain.Property
 import com.example.android.marsrealestate.network.MarsApi
+import com.example.android.marsrealestate.network.MarsPropertiesContainer
 import com.example.android.marsrealestate.network.asDatabaseModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
@@ -19,7 +21,9 @@ class PropertiesRepository(private val database: PropertiesDatabase) {
     suspend fun refreshProperties(filter: String) {
         withContext(Dispatchers.IO) {
             val marsProperties = MarsApi.retrofitService.getProperties(filter)
-            database.databaseDao.insertAll(*marsProperties.asDatabaseModel())
+            val container = MarsPropertiesContainer(marsProperties)
+            Log.i("here", "property type is ${marsProperties[0].type}")
+            database.databaseDao.insertAll(*container.asDatabaseModel())
         }
     }
 
